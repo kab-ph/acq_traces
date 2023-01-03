eb527afb751f145a6adbef816451302caed321e6
Karim Abdellatif <karim.abdellatif@ledger.fr>
2023-01-03 10:14:49 +0100

    add-alignment

--- acq_traces.py
+++ acq_traces.py
@@ -1,8 +1,11 @@
 import numpy as np
 from tqdm import tqdm
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import h5py
 import os
+from scipy import fft
+from scipy import signal
+
 
 
 class acq:
@@ -58,6 +61,30 @@ class Leakeges:
 
 
 
+class alignment:
+    def align_trace(self, ref, trace, norm, visulize):
+
+        self.ref1 = np.concatenate((ref, np.zeros(len(trace) - len(ref))))
+        if (np.std(trace) != 0):
+            if norm:
+                self.ref1 = (self.ref1 - np.mean(self.ref1)) / (np.std(self.ref1) * len(self.ref1))
+                trace = (trace - np.mean(trace)) / (np.std(trace))
+            self.corr = signal.correlate(trace, self.ref1, 'full')
+            self.corr = self.corr[int(len(trace)):int(2 * len(trace))]
+            # corr=corr[0:200]
+            self.max_corr = np.max(self.corr)
+            # print(max_corr)
+            self.peak_position = np.where(self.corr == self.max_corr)[0][0]
+        else:
+            # input('error')
+            self.peak_position = 100
+        if visulize:
+            fig, (ax1, ax2) = plt.subplots(2)
+            ax1.plot(trace)
+            ax2.plot(self.corr)
+            plt.show()
+        # self.trace_out = trace[self.peak_position:self.peak_position + len(ref)]
+        return self.peak_position
 
 
 
@@ -75,6 +102,31 @@ def read_traces():
     return leaks.read()
 
 
+def align_traces():
+    f = h5py.File('19.h5', 'r')
+    f = f['leakages'][:]
+    f = f[:, 10000:32000]
+    # ref = f[0, 1000: 17000]
+    ref = np.load('ref.npy')
+    a = alignment()
+    aligned_traces = np.zeros((f.shape[0], 20600))
+    # indexMax = a.align_trace(ref, f[1], norm=True, visulize=True)
+
+    for x, y in enumerate(f):
+        indexMax = a.align_trace(ref, y, norm=True, visulize=False)
+        # print('index', indexMax)
+        aligned_traces[x] = y[indexMax : indexMax + 20600]
+
+    for i in range(50):
+        plt.plot(aligned_traces[i])
+    plt.show()
+
+
+
+
+
+
 if __name__ == "__main__":
-    traces = read_traces()
+    # traces = read_traces()
     # write_traces()
+    align_traces()
